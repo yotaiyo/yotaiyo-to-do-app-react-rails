a77af5dddf05a2b5f895657f7b2625c07f07ea65
yotaiyo <yota071218@gmail.com>
2019-06-10 02:46:18 +0900

    validate length of name & email

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
     has_secure_password
     has_secure_token
 
-    validates :email, presence: true, uniqueness: true
-    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true, length: { maximum: 255 }
+    validates :name, presence: true, length: { maximum: 50 }
     validates :password_digest, presence: true
     validates :token, uniqueness: true
 end
--- backend/db/migrate/20190609162320_create_users.rb
+++ backend/db/migrate/20190609162320_create_users.rb
@@ -1,14 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :email, null: false
-      t.string :name, null: false
-      t.string :password_digest, null: false
+      t.string :email
+      t.string :name
+      t.string :password_digest
       t.string :token
 
       t.timestamps
     end
-    add_index :users, :email, unique: true
-    add_index :users, :token, unique: true
   end
 end
