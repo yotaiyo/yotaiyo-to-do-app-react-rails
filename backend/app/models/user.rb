8132fe454d3d208d62078b6806f2637bfa886269
yotaiyo <yota071218@gmail.com>
2019-06-10 02:40:31 +0900

    add should be valid & name should be present to user_test

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
     has_secure_password
     has_secure_token
 
-    validates :email, presence: true
-    validates :email, uniqueness: true
+    validates :email, presence: true, uniqueness: true
     validates :name, presence: true
     validates :password_digest, presence: true
     validates :token, uniqueness: true
