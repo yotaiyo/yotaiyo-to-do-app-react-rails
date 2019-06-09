5fe701e676d44344c3261049a53c8ffbb586d369
yotaiyo <yota071218@gmail.com>
2019-06-10 02:53:23 +0900

    validate format of email

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
     has_secure_password
     has_secure_token
 
-    validates :email, presence: true, uniqueness: true, length: { maximum: 255 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    validates :email, presence: true, uniqueness: true, length: { maximum: 255 },
+              format: { with: VALID_EMAIL_REGEX }
     validates :name, presence: true, length: { maximum: 50 }
     validates :password_digest, presence: true
     validates :token, uniqueness: true
