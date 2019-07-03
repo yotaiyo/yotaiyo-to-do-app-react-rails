48830345cae576e050038b90db70c1271f967db7
yotaiyo <yota071218@gmail.com>
2019-07-04 00:59:07 +0900

    standardize-section-header in AboutScreen

--- frontend/pages/AboutScreen.tsx
+++ frontend/pages/AboutScreen.tsx
@@ -1,25 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Header } from '../components/Header'
-import Section from '../components/Section'
 
-const Wrapper = styled.div`
-    display: flex;
-    flex-direction: row;
-    font-family: 'Vollkorn', serif;
-`
-
-const LeftWrapper = styled.div`
-    border-right: solid 1px #CCCCCC;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    height: 1500px;
-`
-
-const RightWrapper = styled.div`
-    flex: 1;
-`
+const Wrapper = styled.div``
 
 const CardWrapper = styled.div`
     box-shadow: 0px 0px 6px 0.05px rgba(0,0,0,0.5);
@@ -41,33 +23,27 @@ const CardBody = styled.div`
 const AboutScreen = () => {
     return (
         <Wrapper>
-            <LeftWrapper>
-                <Section />
-            </LeftWrapper>
-            <RightWrapper>
-                <Header />
-                <CardWrapper>
-                    <CardTitle>yotaiyo-to-do-appとは</CardTitle>
-                    <CardBody>
-                        フロントエンドからバックエンドまでを通して、個人で簡単なアプリケーションを作成できるようになることを目的に作成したToDoアプリです。
-                    </CardBody>
-                    <CardBody>
-                        ToDoの追加、完了、フィルタリング、募集期限の設定、ソートといった機能やトークンベースの認証機能が実装されています。
-                    </CardBody>
-                </CardWrapper>
-                <CardWrapper>
-                    <CardTitle>使用技術</CardTitle>
-                    <CardBody>
-                        フロントエンド: Next.js、バックエンド: Ruby on Railsで構成されています。
-                    </CardBody>
-                </CardWrapper>
-                <CardWrapper>
-                    <CardTitle>ソースコード</CardTitle>
-                    <CardBody>
-                        <a href={'https://github.com/yotaiyo/yotaiyo-to-do-app-react-rails'}>Github</a>で公開しています。
-                    </CardBody>
-                </CardWrapper>
-            </RightWrapper>
+            <CardWrapper>
+                <CardTitle>yotaiyo-to-do-appとは</CardTitle>
+                <CardBody>
+                    フロントエンドからバックエンドまでを通して、個人で簡単なアプリケーションを作成できるようになることを目的に作成したToDoアプリです。
+                </CardBody>
+                <CardBody>
+                    ToDoの追加、完了、フィルタリング、募集期限の設定、ソートといった機能やトークンベースの認証機能が実装されています。
+                </CardBody>
+            </CardWrapper>
+            <CardWrapper>
+                <CardTitle>使用技術</CardTitle>
+                <CardBody>
+                    フロントエンド: Next.js、バックエンド: Ruby on Railsで構成されています。
+                </CardBody>
+            </CardWrapper>
+            <CardWrapper>
+                <CardTitle>ソースコード</CardTitle>
+                <CardBody>
+                    <a href={'https://github.com/yotaiyo/yotaiyo-to-do-app-react-rails'}>Github</a>で公開しています。
+                </CardBody>
+            </CardWrapper>
         </Wrapper>
     )
 }
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,7 +1,28 @@
 import React from 'react'
 import App, { Container } from 'next/app'
+import styled from 'styled-components'
+import { Header } from '../components/Header'
+import Section from '../components/Section'
 import 'react-datepicker/dist/react-datepicker.css'
 import 'react-toastify/dist/ReactToastify.min.css'
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    font-family: 'Vollkorn', serif;
+`
+
+const LeftWrapper = styled.div`
+    border-right: solid 1px #CCCCCC;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    height: 1500px;
+`
+
+const RightWrapper = styled.div`
+    flex: 1;
+`
 
 export default class MyApp extends App {
   static async getInitialProps({ Component, ctx }: { Component: any, ctx: any}) {
@@ -19,7 +40,15 @@ export default class MyApp extends App {
 
     return (
       <Container>
-        <Component {...pageProps} />
+        <Wrapper>
+          <LeftWrapper>
+            <Section />
+          </LeftWrapper>
+          <RightWrapper>
+            <Header />
+            <Component {...pageProps} />
+          </RightWrapper>
+        </Wrapper>
       </Container>
     )
   }
